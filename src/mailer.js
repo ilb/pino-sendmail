dca3df1c90b5afe989b3a7a67918452116eefcd7
Vjacheslav Borisov <slavb@ilb.ru>
2021-10-28 08:11:55 +0400

    remove debug

--- src/mailer.js
+++ src/mailer.js
@@ -3,9 +3,7 @@ const nodemailer = require('nodemailer');
 
 const config = process.env.MAIL_URL || {
   sendmail: true,
-  path: process.env.MAIL_COMMAND || '/usr/sbin/sendmail',
-  debug: process.env.MAIL_DEBUG || true,
-  log: process.env.MAIL_LOG || true
+  path: process.env.MAIL_COMMAND || '/usr/sbin/sendmail'
 };
 const mailer = nodemailer.createTransport(config);
 
